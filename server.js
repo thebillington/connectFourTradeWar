692b4bb60160017e8361d98bbae522336157d010
Billy Rebecchi <billy.rebecchi@googlemail.com>
2019-09-27 10:20:44 +0100

    Create lobby system

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ var server = http.Server(app);
 
 var io = socketIO(server);app.set('port', 5000);
 
-var objects = [];
+var lobbies = [];
+
+var objects = [
+  Rectangle(10, 80, 0, 0, 15, 15, "grey", "turret"),
+  Rectangle(10, 80, 0, 0, 15, 15, "grey", "turret")
+];
 
 app.use('/static', express.static(__dirname + '/static'));
 
@@ -22,16 +27,39 @@ setInterval(function() {
     io.sockets.emit('objects', objects);
   }, 1000/20);
 
+  setInterval(function() {
+      io.sockets.emit('lobbies', lobbies);
+    }, 1000);
+
 io.on("connection", function(connection) {
-  connection.on("click", function(data) {
-    objects.push(Circle(data[0],data[1],0,0,5,"red"));
+  connection.on("newLobby", function(username) {
+    lobbies.push(Lobby(connection.id, username));
+  });
+  connection.on("joinLobby", function(_host) {
+    var inLobby = false;
+    for (var i = 0; i < lobbies.length; i++) {
+      if (lobbies[i]. host == _host) {
+        lobbies[i].full = true;
+        lobbies[i].playerID = connection.id;
+        inLobby = true;
+      }
+      break;
+    }
+    connection.emit("inLobby", inLobby);
+  });
+  connection.on("fire", function(x,y, dx, dy) {
+    objects.push(Circle(x,y,dx,dy,2,"red"));
   });
 });
 
-function Circle(_x, _y, _dx, _dy, _radius, _colour) {
-  return {x: _x, y: _y, dx: _dx, dy: _dy, r: _radius, c: _colour, type: "circle"};
+function Lobby(connectionID, hostName) {
+  return {hostID: connectionID, playerID: null, host: hostName, full: false};
 }
 
-function Rectangle(_x, _y, _dx, _dy, _width, _height, _colour) {
-  return {x: _x, y: _y, dx: _dx, dy: _dy, w: _width, h: _height, c: _colour, type: "rect"};
+function Circle(_x, _y, _dx, _dy, _radius, _colour, _id) {
+  return {x: _x, y: _y, dx: _dx, dy: _dy, r: _radius, c: _colour, type: "circle", id: _id};
+}
+
+function Rectangle(_x, _y, _dx, _dy, _width, _height, _colour, _id) {
+  return {x: _x, y: _y, dx: _dx, dy: _dy, w: _width, h: _height, c: _colour, type: "rect", id: _id};
 }
--- static/game.js
+++ static/game.js
@@ -1,14 +1,31 @@
 var socket = io();
 var objects = [];
+var lobbies = [];
+var username;
+
+var inLobby = false;
+
+socket.on('lobbies', function(_lobbies) {
+  lobbies = _lobbies;
+  updateLobbies();
+});
 
 socket.on('objects', function(_objects) {
   objects = _objects;
+});
+
+socket.on('inLobby', function(_inLobby) {
+  inLobby = inLobby;
+  if (inLobby) {
+    document.getElementById('createLobby').style.display = "none";
+  }
 });
 
 function setup() {
   createCanvas(windowHeight,windowHeight);
   document.getElementById('lobby').style.width = windowWidth - windowHeight - 4;
   document.getElementById('lobby').style.height = windowHeight - 4;
+  document.getElementById('username').value = newID();
 }
 
 function draw() {
@@ -30,5 +47,35 @@ function drawCircle(circle) {
 }
 
 function mouseClicked() {
-  socket.emit("click", [mouseX, mouseY]);
+  socket.emit("fire", mouseX, mouseY, 0, 0);
 }
+
+function updateLobbies() {
+  var lobbyText = "<ul>";
+  for (var i = 0; i < lobbies.length; i++) {
+    lobbyText += "<li>" + lobbies[i].host;
+    if (!lobbies[i].full && !inLobby) {
+      lobbyText += ": <button onclick=joinLobby('" + lobbies[i].host + "')>Join</button>";
+    }
+    lobbyText += "</li>";
+  }
+  lobbyText += "</ul>";
+  document.getElementById('lobbies').innerHTML = lobbyText;
+}
+
+function createLobby() {
+  if (!inLobby) {
+    username = document.getElementById('username').value;
+    socket.emit("newLobby", username);
+    inLobby = true;
+    document.getElementById('createLobby').style.display = "none";
+  }
+}
+
+function joinLobby(host) {
+  socket.emit('joinLobby', host);
+}
+
+function newID() {
+  return '_' + Math.random().toString(36).substr(2, 9);
+};
